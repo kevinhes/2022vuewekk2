db993aeb0498bc68d63ae201ee075be40ead1347
kevinhes <kevinhes@hotmail.com>
2022-01-17 10:14:13 +0800

    改使用 vue

--- JS/backstage.js
+++ JS/backstage.js
@@ -1,2 +1,78 @@
 // 取得 cookie
 
+const token = document.cookie.replace(/(?:(?:^|.*;\s*)hextoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+axios.defaults.headers.common['Authorization'] = token
+
+// 驗證是否登入
+
+function checkLoginStatus() {
+    if(token == ''){
+        alert('您尚未登入')
+        window.location = './login.html'
+    }
+}
+
+// 測試是否登入
+
+const checkLoginBtn = document.querySelector('.checkLogin')
+
+checkLoginBtn.addEventListener('click', e=> {
+    axios.post(`${api_url}/v2/api/user/check`)
+        .then(res => {
+            if(res.data.success === true){
+                alert('正在登入狀態')
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+})
+
+// 動元素
+
+const productList = document.querySelector('#productList')
+
+// 初始化
+
+let productsData = []
+
+function init() {
+    getProdutsList()
+    checkLoginStatus()
+}
+
+init()
+
+// 取得產品列表
+
+function getProdutsList() {
+    axios.get(`${api_url}/v2/api/${api_path}/admin/products`)
+        .then(res => {
+            productsData = res.data.products
+            console.log(productsData);
+            let str = ''
+            productsData.forEach( i => {
+                str += `
+                <tr>
+                    <td>${i.title}</td>
+                    <td width="120">
+                    ${i.origin_price}
+                    </td>
+                    <td width="120">
+                    ${i.price}
+                    </td>
+                    <td width="100">
+                    <span class="">${i.is_enabled ? '啟用':'未啟用'}</span>
+                    </td>
+                    <td width="120">
+                    <button type="button" class="btn btn-sm btn-outline-danger move deleteBtn" data-action="remove" data-id="${i.id}"> 刪除 </button>
+                    </td>
+                </tr>
+                `
+            })
+            productList.innerHTML = str
+        })
+        .catch(error => {
+            console.log(error.response);
+        })
+}
--- JS/login.js
+++ JS/login.js
@@ -1,32 +1,66 @@
-// 動元素
-
-const userName = document.querySelector('#username')
-const password = document.querySelector('#password')
-const loginBtn = document.querySelector('#form button')
-
-// 監聽
-
-loginBtn.addEventListener('click', e => {
-  e.preventDefault()
-  if(userName.value != '' && password.value != ''){
-    let user = {
-      "username": userName.value,
-      "password": password.value
+const app = Vue.createApp({
+  data() {
+    return {
+      api_path : 'kevinhesapi',
+      api_url : 'https://vue3-course-api.hexschool.io/',
+      user:{
+        "username": '',
+        "password": ''
+      }
     }
-    axios.post(`${api_url}/v2/admin/signin`,user)
-      .then(res => {
-        console.log(res);
-        let {token, expired} = res.data
-        console.log(token,expired);
-        document.cookie = `hextoken=${token}; expires=${new Date(expired)}`
-        window.location = './backstage.html'
-      })
-      .catch(error => {
-        console.dir(error);
-      })
-  }else {
-    alert('請輸入帳密')
+  },
+  methods: {
+    login() {
+      if(this.user.username != '' && this.user.password){
+      axios.post(`${this.api_url}/v2/admin/signin`, this.user)
+        .then(res => {
+          console.log(res);
+          let {token, expired} = res.data
+          console.log(token,expired);
+          document.cookie = `hextoken=${token}; expires=${new Date(expired)}`
+          window.location = './backstage.html'
+        })
+        .catch(error => {
+          console.dir(error);
+        })
+      } else {
+        alert('請輸入帳號密碼')
+      }
+    }
   }
 })
 
-// 登入函式
+app.mount('#app')
+
+// // 動元素
+
+// const userName = document.querySelector('#username')
+// const password = document.querySelector('#password')
+// const loginBtn = document.querySelector('#form button')
+
+// // 監聽
+
+// loginBtn.addEventListener('click', e => {
+//   e.preventDefault()
+//   if(userName.value != '' && password.value != ''){
+//     let user = {
+//       "username": userName.value,
+//       "password": password.value
+//     }
+//     axios.post(`${api_url}/v2/admin/signin`,user)
+//       .then(res => {
+//         console.log(res);
+//         let {token, expired} = res.data
+//         console.log(token,expired);
+//         document.cookie = `hextoken=${token}; expires=${new Date(expired)}`
+//         window.location = './backstage.html'
+//       })
+//       .catch(error => {
+//         console.dir(error);
+//       })
+//   }else {
+//     alert('請輸入帳密')
+//   }
+// })
+
+// // 登入函式
